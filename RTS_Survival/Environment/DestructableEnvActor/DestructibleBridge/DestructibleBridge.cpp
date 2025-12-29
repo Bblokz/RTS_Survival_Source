81648b84e246de4394150c68582414405674d79c
Bas13900k <basblokz@gmail.com>
2025-12-29 16:18:53 +0100

    bridge code

---
+++ RTS_Survival/Environment/DestructableEnvActor/DestructibleBridge/DestructibleBridge.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (C) Bas Blokzijl - All rights reserved.
+
+
+#include "DestructibleBridge.h"
+
+#include "RTS_Survival/RTSCollisionTraceChannels.h"
+
+
+ADestructibleBridge::ADestructibleBridge(const FObjectInitializer& ObjectInitializer)
+	: ADestructableEnvActor(ObjectInitializer)
+{
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ADestructibleBridge::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADestructibleBridge::KillActorsOnBridge(UMeshComponent* BridgeMesh)
+{
+	if (not IsValid(BridgeMesh))
+	{
+		return;
+	}
+	TArray<ECollisionChannel> CollisionChannelsToCheck =
+	{
+		ECC_Pawn,
+		ECC_PhysicsBody,
+		ECC_Destructible,
+		COLLISION_OBJ_PLAYER,
+		COLLISION_OBJ_ENEMY
+	};
+}
---
+++ RTS_Survival/Environment/DestructableEnvActor/DestructibleBridge/DestructibleBridge.h
@@ -0,0 +1,25 @@
+﻿// Copyright (C) Bas Blokzijl - All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTS_Survival/Environment/DestructableEnvActor/DestructableEnvActor.h"
+#include "DestructibleBridge.generated.h"
+
+UCLASS()
+class RTS_SURVIVAL_API ADestructibleBridge : public ADestructableEnvActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ADestructibleBridge(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, NotBlueprintable)
+	void KillActorsOnBridge(UMeshComponent* BridgeMesh);
+
+
+};
