b245e580e527779691a88c0864d1cb6bbf02410f
Bas13900k <basblokz@gmail.com>
2026-01-23 16:43:49 +0100

    Update on RTS_Survival/Game/Difficulty/

---
+++ RTS_Survival/Game/Difficulty/GameDifficulty.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameDifficulty.generated.h"
+
+
+UENUM()
+enum class ERTSGameDifficulty
+{
+	NewToRTS,
+	Normal,
+	Hard,
+	Brutal,
+	Ironman
+};
+
+USTRUCT(Blueprintable)
+struct FRTSGameDifficulty
+{
+	UPROPERTY(BlueprintReadWrite)
+	int32 DifficultyPercentage  =0;
+
+	UPROPERTY(BlueprintReadWrite)
+	ERTSGameDifficulty DifficultyLevel = ERTSGameDifficulty::Normal;
+	
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsInitialized = false;
+};
---
+++ RTS_Survival/Game/Difficulty/GameDifficultySettings/RTSGameDifficultySettings.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) Bas Blokzijl - All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "RTSGameDifficultySettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RTS_SURVIVAL_API URTSGameDifficultySettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+};
