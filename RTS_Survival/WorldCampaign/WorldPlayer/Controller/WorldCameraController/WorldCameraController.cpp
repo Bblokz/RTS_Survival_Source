20fb02e1bc8a0b1338f734138761ae8098a64fe3
Bas Blokzijl <79595891+Bblokz@users.noreply.github.com>
2026-02-02 23:05:48 +0100

    Add world campaign camera control routing

--- RTS_Survival/WorldCampaign/WorldPlayer/Controller/WorldCameraController/WorldCameraController.cpp
+++ RTS_Survival/WorldCampaign/WorldPlayer/Controller/WorldCameraController/WorldCameraController.cpp
@@ -3,35 +3,264 @@
 
 #include "WorldCameraController.h"
 
-namespace WorldCameraSettings
-{
-	constexpr float XYSpeed = 800.f;
-	constexpr float ZoomSpeed  = 300.f;
-}
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "RTS_Survival/Utils/HFunctionLibary.h"
 
-// Sets default values for this component's properties
 UWorldCameraController::UWorldCameraController()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 	PrimaryComponentTick.bStartWithTickEnabled = true;
 }
 
-
-// Called when the game starts
 void UWorldCameraController::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	BeginPlay_InitCameraCarrier();
+	BeginPlay_InitSpringArmComponent();
 }
 
-
-// Called every frame
 void UWorldCameraController::TickComponent(float DeltaTime, ELevelTick TickType,
                                            FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	UpdateCameraInputDisabled(DeltaTime);
+	UpdateMoveToLocation(DeltaTime);
+	UpdateAxisMovement(DeltaTime);
 }
 
+void UWorldCameraController::SetIsCameraMovementDisabled(const bool bIsDisabled)
+{
+	bM_IsCameraMovementDisabled = bIsDisabled;
+}
+
+bool UWorldCameraController::GetIsCameraMovementDisabled() const
+{
+	return bM_IsCameraMovementDisabled || M_CameraInputDisabledRemainingSeconds > 0.0f;
+}
+
+void UWorldCameraController::ZoomIn()
+{
+	if (GetIsCameraMovementDisabled())
+	{
+		return;
+	}
+
+	if (GetIsValidSpringArmComponent())
+	{
+		const float NewTargetLength = FMath::Clamp(M_SpringArmComponent->TargetArmLength - M_ZoomSpeed, M_MinZoom, M_MaxZoom);
+		M_SpringArmComponent->TargetArmLength = NewTargetLength;
+		return;
+	}
+
+	if (not GetIsValidCameraCarrierActor())
+	{
+		return;
+	}
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	const FVector CurrentLocation = CameraCarrier->GetActorLocation();
+	const FVector NewLocation = CurrentLocation + FVector(0.0f, 0.0f, -M_ZoomSpeed);
+	CameraCarrier->SetActorLocation(NewLocation, true);
+}
+
+void UWorldCameraController::ZoomOut()
+{
+	if (GetIsCameraMovementDisabled())
+	{
+		return;
+	}
+
+	if (GetIsValidSpringArmComponent())
+	{
+		const float NewTargetLength = FMath::Clamp(M_SpringArmComponent->TargetArmLength + M_ZoomSpeed, M_MinZoom, M_MaxZoom);
+		M_SpringArmComponent->TargetArmLength = NewTargetLength;
+		return;
+	}
+
+	if (not GetIsValidCameraCarrierActor())
+	{
+		return;
+	}
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	const FVector CurrentLocation = CameraCarrier->GetActorLocation();
+	const FVector NewLocation = CurrentLocation + FVector(0.0f, 0.0f, M_ZoomSpeed);
+	CameraCarrier->SetActorLocation(NewLocation, true);
+}
+
+void UWorldCameraController::ForwardRightMovement(const bool bOnForward, const float AxisX, const bool bOnRight, const float AxisY)
+{
+	M_AxisInputState.bM_IsForwardInputActive = bOnForward;
+	M_AxisInputState.M_ForwardAxisValue = AxisX;
+	M_AxisInputState.bM_IsRightInputActive = bOnRight;
+	M_AxisInputState.M_RightAxisValue = AxisY;
+}
+
+void UWorldCameraController::MoveCameraTo(const FMovePlayerCamera& MoveRequest)
+{
+	if (not GetIsValidCameraCarrierActor())
+	{
+		return;
+	}
+
+	if (IsValid(MoveRequest.MoveSound))
+	{
+		UGameplayStatics::PlaySound2D(this, MoveRequest.MoveSound);
+	}
+
+	M_CameraInputDisabledRemainingSeconds = FMath::Max(
+		M_CameraInputDisabledRemainingSeconds,
+		MoveRequest.TimeCameraInputDisabled
+	);
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	if (MoveRequest.TimeToMove <= 0.0f)
+	{
+		CameraCarrier->SetActorLocation(MoveRequest.MoveToLocation, true);
+		M_MoveState.bM_IsMoveActive = false;
+		return;
+	}
+
+	M_MoveState.M_StartLocation = CameraCarrier->GetActorLocation();
+	M_MoveState.M_TargetLocation = MoveRequest.MoveToLocation;
+	M_MoveState.M_ElapsedTime = 0.0f;
+	M_MoveState.M_TotalTime = MoveRequest.TimeToMove;
+	M_MoveState.bM_IsMoveActive = true;
+}
+
+void UWorldCameraController::BeginPlay_InitCameraCarrier()
+{
+	AActor* const OwnerActor = GetOwner();
+	if (not IsValid(OwnerActor))
+	{
+		return;
+	}
+
+	if (APlayerController* const PlayerController = Cast<APlayerController>(OwnerActor))
+	{
+		if (APawn* const PossessedPawn = PlayerController->GetPawn())
+		{
+			M_CameraCarrierActor = PossessedPawn;
+			return;
+		}
+	}
+
+	M_CameraCarrierActor = OwnerActor;
+}
+
+void UWorldCameraController::BeginPlay_InitSpringArmComponent()
+{
+	if (not GetIsValidCameraCarrierActor())
+	{
+		return;
+	}
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	M_SpringArmComponent = CameraCarrier->FindComponentByClass<USpringArmComponent>();
+}
+
+void UWorldCameraController::UpdateCameraInputDisabled(const float DeltaTime)
+{
+	if (M_CameraInputDisabledRemainingSeconds <= 0.0f)
+	{
+		M_CameraInputDisabledRemainingSeconds = 0.0f;
+		return;
+	}
+
+	M_CameraInputDisabledRemainingSeconds = FMath::Max(M_CameraInputDisabledRemainingSeconds - DeltaTime, 0.0f);
+}
+
+void UWorldCameraController::UpdateMoveToLocation(const float DeltaTime)
+{
+	if (not M_MoveState.bM_IsMoveActive)
+	{
+		return;
+	}
+
+	if (not GetIsValidCameraCarrierActor())
+	{
+		M_MoveState.bM_IsMoveActive = false;
+		return;
+	}
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	if (M_MoveState.M_TotalTime <= 0.0f)
+	{
+		CameraCarrier->SetActorLocation(M_MoveState.M_TargetLocation, true);
+		M_MoveState.bM_IsMoveActive = false;
+		return;
+	}
+
+	M_MoveState.M_ElapsedTime += DeltaTime;
+	const float Alpha = FMath::Clamp(M_MoveState.M_ElapsedTime / M_MoveState.M_TotalTime, 0.0f, 1.0f);
+	const FVector NewLocation = FMath::Lerp(M_MoveState.M_StartLocation, M_MoveState.M_TargetLocation, Alpha);
+	CameraCarrier->SetActorLocation(NewLocation, true);
+
+	if (Alpha >= 1.0f)
+	{
+		CameraCarrier->SetActorLocation(M_MoveState.M_TargetLocation, true);
+		M_MoveState.bM_IsMoveActive = false;
+	}
+}
+
+void UWorldCameraController::UpdateAxisMovement(const float DeltaTime)
+{
+	if (GetIsCameraMovementDisabled())
+	{
+		return;
+	}
+
+	if (not GetIsValidCameraCarrierActor())
+	{
+		return;
+	}
+
+	const float ForwardAxisValue = M_AxisInputState.bM_IsForwardInputActive ? M_AxisInputState.M_ForwardAxisValue : 0.0f;
+	const float RightAxisValue = M_AxisInputState.bM_IsRightInputActive ? M_AxisInputState.M_RightAxisValue : 0.0f;
+	if (ForwardAxisValue == 0.0f && RightAxisValue == 0.0f)
+	{
+		return;
+	}
+
+	AActor* const CameraCarrier = M_CameraCarrierActor.Get();
+	const FVector ForwardVector = CameraCarrier->GetActorForwardVector();
+	const FVector RightVector = CameraCarrier->GetActorRightVector();
+	const FVector WorldDelta = (ForwardVector * ForwardAxisValue + RightVector * RightAxisValue) * M_XYSpeed * DeltaTime;
+	CameraCarrier->AddActorWorldOffset(WorldDelta, true);
+}
+
+bool UWorldCameraController::GetIsValidCameraCarrierActor() const
+{
+	if (M_CameraCarrierActor.IsValid())
+	{
+		return true;
+	}
+
+	RTSFunctionLibrary::ReportErrorVariableNotInitialised_Object(
+		this,
+		TEXT("M_CameraCarrierActor"),
+		TEXT("GetIsValidCameraCarrierActor"),
+		this
+	);
+	return false;
+}
+
+bool UWorldCameraController::GetIsValidSpringArmComponent() const
+{
+	if (M_SpringArmComponent.IsValid())
+	{
+		return true;
+	}
+
+	RTSFunctionLibrary::ReportErrorVariableNotInitialised_Object(
+		this,
+		TEXT("M_SpringArmComponent"),
+		TEXT("GetIsValidSpringArmComponent"),
+		this
+	);
+	return false;
+}
--- RTS_Survival/WorldCampaign/WorldPlayer/Controller/WorldPlayerController.cpp
+++ RTS_Survival/WorldCampaign/WorldPlayer/Controller/WorldPlayerController.cpp
@@ -2,3 +2,104 @@
 
 
 #include "WorldPlayerController.h"
+
+#include "RTS_Survival/Utils/HFunctionLibary.h"
+#include "WorldCameraController/WorldCameraController.h"
+
+void AWorldPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UWorldCameraController* FoundController = nullptr;
+	if (APawn* const PossessedPawn = GetPawn())
+	{
+		FoundController = PossessedPawn->FindComponentByClass<UWorldCameraController>();
+	}
+
+	if (not IsValid(FoundController))
+	{
+		FoundController = FindComponentByClass<UWorldCameraController>();
+	}
+
+	M_WorldCameraController = FoundController;
+}
+
+void AWorldPlayerController::SetIsWorldCameraMovementDisabled(const bool bIsDisabled)
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return;
+	}
+
+	M_WorldCameraController->SetIsCameraMovementDisabled(bIsDisabled);
+}
+
+bool AWorldPlayerController::GetIsWorldCameraMovementDisabled() const
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return false;
+	}
+
+	return M_WorldCameraController->GetIsCameraMovementDisabled();
+}
+
+void AWorldPlayerController::WorldCamera_ZoomIn()
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return;
+	}
+
+	M_WorldCameraController->ZoomIn();
+}
+
+void AWorldPlayerController::WorldCamera_ZoomOut()
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return;
+	}
+
+	M_WorldCameraController->ZoomOut();
+}
+
+void AWorldPlayerController::WorldCamera_ForwardRightMovement(
+	const bool bOnForward,
+	const float AxisX,
+	const bool bOnRight,
+	const float AxisY)
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return;
+	}
+
+	M_WorldCameraController->ForwardRightMovement(bOnForward, AxisX, bOnRight, AxisY);
+}
+
+void AWorldPlayerController::WorldCamera_MoveTo(const FMovePlayerCamera& MoveRequest)
+{
+	if (not GetIsValidWorldCameraController())
+	{
+		return;
+	}
+
+	M_WorldCameraController->MoveCameraTo(MoveRequest);
+}
+
+bool AWorldPlayerController::GetIsValidWorldCameraController() const
+{
+	if (M_WorldCameraController.IsValid())
+	{
+		return true;
+	}
+
+	RTSFunctionLibrary::ReportErrorVariableNotInitialised(
+		this,
+		TEXT("M_WorldCameraController"),
+		TEXT("GetIsValidWorldCameraController"),
+		this
+	);
+	return false;
+}
