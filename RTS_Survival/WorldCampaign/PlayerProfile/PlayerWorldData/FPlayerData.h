34df603856bdc5aea472b49c48f1c87039a698be
Bas13900k <basblokz@gmail.com>
2026-02-06 10:52:52 +0100

    Update in RTS_Survival/WorldCampaign/PlayerProfile/PlayerWorldData/FPlayerData.h

---
+++ RTS_Survival/WorldCampaign/PlayerProfile/PlayerWorldData/FPlayerData.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "ArchiveSaveData/FArchiveSaveData.h"
+#include "RTS_Survival/WorldCampaign/PlayerProfile/PlayerCardData/PlayerCardData.h"
+#include "RTS_Survival/WorldCampaign/WorldMapUI/PerkSystem/PerkTypes.h"
+#include "RTS_Survival/WorldCampaign/WorldMapUI/PerkSystem/PlayerRank/PlayerRanks.h"
+
+#include "FPlayerData.generated.h"
+
+enum class ERTSFaction : uint8;
+
+USTRUCT(BlueprintType)
+struct FPlayerPerkSaveData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	int32 UnspentPerkPoints = 0;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	FPlayerRankProgress RankProgress;
+
+	// Contains one progress data struct for each perk type supported in the UI.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	TArray<FPlayerPerkProgress> PerkProgress;
+
+};
+
+USTRUCT(BlueprintType)
+struct FPlayerData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	FPlayerPerkSaveData PerkData;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	FArchiveSaveData ArchiveData;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
+	FPlayerCardSaveData CardData;
+	
+	
+};
