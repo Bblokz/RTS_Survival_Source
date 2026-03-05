1f18a9894ffcb460d89f80558b1aaaec1d858a5a
Bas Blokzijl <79595891+Bblokz@users.noreply.github.com>
2026-03-05 15:14:51 +0100

    Restrict anim mesh feedback to pitch-only rotation

---
+++ RTS_Survival/RTSComponents/VehicleFireFeedbackComponent/AnimMeshFeedbackComponent.cpp
@@ -0,0 +1,68 @@
+#include "AnimMeshFeedbackComponent.h"
+
+#include "RTS_Survival/RTSUtilities/RTSFunctionLibrary/RTSFunctionLibrary.h"
+
+void UAnimMeshFeedbackComponent::InitializeAnimMeshFeedbackComponent(
+	USkeletalMeshComponent* InAnimatedMesh,
+	ACPPTurretsMaster* InTurretMaster)
+{
+	M_AnimatedMesh = InAnimatedMesh;
+
+	Super::InitializeFeedbackComponent(
+		InAnimatedMesh,
+		nullptr,
+		InTurretMaster);
+}
+
+bool UAnimMeshFeedbackComponent::GetIsValidHullMesh() const
+{
+	return GetIsValidAnimatedMesh();
+}
+
+void UAnimMeshFeedbackComponent::CacheHullBaseTransform()
+{
+	if (not GetIsValidAnimatedMesh())
+	{
+		return;
+	}
+
+	M_BaseHullRelativeRotation = M_AnimatedMesh->GetRelativeRotation();
+}
+
+void UAnimMeshFeedbackComponent::ApplyFeedbackKick(
+	const float NormalisedMuzzleEnergy,
+	const float TurretWorldYawDegrees)
+{
+	(void)TurretWorldYawDegrees;
+
+	const float ClampedEnergy = FMath::Clamp(NormalisedMuzzleEnergy, 0.0f, 1.0f);
+	M_RecoilRotDeg.X += M_FeedbackSettings.M_MaxPitchDeg * ClampedEnergy;
+}
+
+void UAnimMeshFeedbackComponent::ApplyHullFeedbackTransform() const
+{
+	if (not GetIsValidAnimatedMesh())
+	{
+		return;
+	}
+
+	const FRotator NewRotation = M_BaseHullRelativeRotation +
+		FRotator(M_RecoilRotDeg.X, 0.0f, 0.0f);
+
+	M_AnimatedMesh->SetRelativeRotation(NewRotation);
+}
+
+bool UAnimMeshFeedbackComponent::GetIsValidAnimatedMesh() const
+{
+	if (IsValid(M_AnimatedMesh))
+	{
+		return true;
+	}
+
+	RTSFunctionLibrary::ReportErrorVariableNotInitialised_Object(
+		this,
+		"M_AnimatedMesh",
+		"GetIsValidAnimatedMesh",
+		this);
+	return false;
+}
---
+++ RTS_Survival/RTSComponents/VehicleFireFeedbackComponent/AnimMeshFeedbackComponent.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTS_Survival/RTSComponents/VehicleFireFeedbackComponent/VehicleFireFeedbackComponent.h"
+#include "AnimMeshFeedbackComponent.generated.h"
+
+class ACPPTurretsMaster;
+class USkeletalMeshComponent;
+
+/**
+ * @brief Allows turrets to drive recoil feedback on a single skeletal mesh using the same weapon-tracking flow.
+ * Use this when no separate hull static mesh exists and only pitch animation should be applied to avoid movement interference.
+ * @note InitializeAnimMeshFeedbackComponent: call in blueprint with animated skeletal mesh + turret master.
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class RTS_SURVIVAL_API UAnimMeshFeedbackComponent : public UVehicleFireFeedbackComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category="VehicleFireFeedback")
+	void InitializeAnimMeshFeedbackComponent(
+		USkeletalMeshComponent* InAnimatedMesh,
+		ACPPTurretsMaster* InTurretMaster);
+
+protected:
+	virtual bool GetIsValidHullMesh() const override;
+	virtual void CacheHullBaseTransform() override;
+	virtual void ApplyFeedbackKick(float NormalisedMuzzleEnergy, float TurretWorldYawDegrees) override;
+	virtual void ApplyHullFeedbackTransform() const override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<USkeletalMeshComponent> M_AnimatedMesh = nullptr;
+
+	bool GetIsValidAnimatedMesh() const;
+};
