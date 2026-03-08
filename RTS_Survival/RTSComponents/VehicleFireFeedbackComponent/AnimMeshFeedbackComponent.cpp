0401f01d651323aa21a3242ad40328f3534eb409
Bas Blokzijl <79595891+Bblokz@users.noreply.github.com>
2026-03-08 10:58:32 +0100

    Expand anim mesh recoil with preserved translation and optional roll

--- RTS_Survival/RTSComponents/VehicleFireFeedbackComponent/AnimMeshFeedbackComponent.cpp
+++ RTS_Survival/RTSComponents/VehicleFireFeedbackComponent/AnimMeshFeedbackComponent.cpp
@@ -26,7 +26,9 @@ void UAnimMeshFeedbackComponent::CacheHullBaseTransform()
 		return;
 	}
 
+	M_LastAppliedRecoilLocationOffsetCm = FVector::ZeroVector;
 	M_LastAppliedRecoilPitchDeg = 0.0f;
+	M_LastAppliedRecoilRollDeg = 0.0f;
 }
 
 void UAnimMeshFeedbackComponent::ApplyFeedbackKick(
@@ -36,7 +38,38 @@ void UAnimMeshFeedbackComponent::ApplyFeedbackKick(
 	(void)TurretWorldYawDegrees;
 
 	const float ClampedEnergy = FMath::Clamp(NormalisedMuzzleEnergy, 0.0f, 1.0f);
+	const FVector BackwardAxis = GetSignedAxisVector(M_AnimMeshRecoilAxesSettings.M_BackwardRecoilAxis);
+	const FVector BackwardRecoilOffset =
+		BackwardAxis * (M_FeedbackSettings.M_MaxBackCm * ClampedEnergy);
+	M_RecoilOffsetCm += BackwardRecoilOffset;
+
+	if (M_AnimMeshRecoilAxesSettings.bM_EnableSideSway)
+	{
+		const FVector SideSwayAxis = GetSignedAxisVector(M_AnimMeshRecoilAxesSettings.M_SideSwayAxis);
+		const float SideSwayJitterCm = FMath::FRandRange(
+			-M_AnimMeshRecoilAxesSettings.M_MaxSideSwayCm,
+			M_AnimMeshRecoilAxesSettings.M_MaxSideSwayCm) * ClampedEnergy;
+		M_RecoilOffsetCm += SideSwayAxis * SideSwayJitterCm;
+	}
+
 	M_RecoilRotDeg.X += M_FeedbackSettings.M_MaxPitchDeg * ClampedEnergy;
+
+	if (not M_AnimMeshRecoilAxesSettings.bM_EnableOptionalRoll)
+	{
+		return;
+	}
+
+	const float RollJitterDeg = FMath::FRandRange(
+		-M_AnimMeshRecoilAxesSettings.M_MaxOptionalRollDeg,
+		M_AnimMeshRecoilAxesSettings.M_MaxOptionalRollDeg) * ClampedEnergy;
+
+	if (M_AnimMeshRecoilAxesSettings.M_OptionalRollChannel == EAnimMeshOptionalRollChannel::Roll)
+	{
+		M_RecoilRotDeg.Z += RollJitterDeg;
+		return;
+	}
+
+	M_RecoilRotDeg.X += RollJitterDeg;
 }
 
 void UAnimMeshFeedbackComponent::ApplyHullFeedbackTransform() const
@@ -46,17 +79,8 @@ void UAnimMeshFeedbackComponent::ApplyHullFeedbackTransform() const
 		return;
 	}
 
-	const FRotator CurrentRelativeRotation = M_AnimatedMesh->GetRelativeRotation();
-	const float BasePitchWithoutRecoil = CurrentRelativeRotation.Pitch - M_LastAppliedRecoilPitchDeg;
-	const float RecoilPitchToApply = M_RecoilRotDeg.X;
-
-	const FRotator NewRotation(
-		BasePitchWithoutRecoil + RecoilPitchToApply,
-		CurrentRelativeRotation.Yaw,
-		CurrentRelativeRotation.Roll);
-
-	M_AnimatedMesh->SetRelativeRotation(NewRotation);
-	M_LastAppliedRecoilPitchDeg = RecoilPitchToApply;
+	ApplyPreservedRecoilLocation();
+	ApplyPreservedRecoilRotation();
 }
 
 bool UAnimMeshFeedbackComponent::GetIsValidAnimatedMesh() const
@@ -72,4 +96,71 @@ bool UAnimMeshFeedbackComponent::GetIsValidAnimatedMesh() const
 		"GetIsValidAnimatedMesh",
 		this);
 	return false;
+}
+
+FVector UAnimMeshFeedbackComponent::GetSignedAxisVector(const EAnimMeshRecoilSignedAxis Axis) const
+{
+	switch (Axis)
+	{
+		case EAnimMeshRecoilSignedAxis::ForwardX:
+			return FVector::ForwardVector;
+		case EAnimMeshRecoilSignedAxis::BackwardX:
+			return -FVector::ForwardVector;
+		case EAnimMeshRecoilSignedAxis::RightY:
+			return FVector::RightVector;
+		case EAnimMeshRecoilSignedAxis::LeftY:
+			return -FVector::RightVector;
+		case EAnimMeshRecoilSignedAxis::UpZ:
+			return FVector::UpVector;
+		case EAnimMeshRecoilSignedAxis::DownZ:
+			return -FVector::UpVector;
+		default:
+			return -FVector::ForwardVector;
+	}
+}
+
+void UAnimMeshFeedbackComponent::ApplyPreservedRecoilLocation() const
+{
+	const FVector CurrentRelativeLocation = M_AnimatedMesh->GetRelativeLocation();
+	const FVector BaseLocationWithoutRecoil = CurrentRelativeLocation - M_LastAppliedRecoilLocationOffsetCm;
+	const FVector RecoilLocationToApply = M_RecoilOffsetCm;
+	const FVector NewRelativeLocation = BaseLocationWithoutRecoil + RecoilLocationToApply;
+
+	M_AnimatedMesh->SetRelativeLocation(NewRelativeLocation);
+	M_LastAppliedRecoilLocationOffsetCm = RecoilLocationToApply;
+}
+
+void UAnimMeshFeedbackComponent::ApplyPreservedRecoilRotation() const
+{
+	const FRotator CurrentRelativeRotation = M_AnimatedMesh->GetRelativeRotation();
+	const float BasePitchWithoutRecoil = CurrentRelativeRotation.Pitch - M_LastAppliedRecoilPitchDeg;
+	const float BaseRollWithoutRecoil = CurrentRelativeRotation.Roll - M_LastAppliedRecoilRollDeg;
+
+	float RecoilPitchToApplyDeg = 0.0f;
+	float RecoilRollToApplyDeg = 0.0f;
+	GetCurrentRecoilPitchAndRoll(RecoilPitchToApplyDeg, RecoilRollToApplyDeg);
+
+	const FRotator NewRotation(
+		BasePitchWithoutRecoil + RecoilPitchToApplyDeg,
+		CurrentRelativeRotation.Yaw,
+		BaseRollWithoutRecoil + RecoilRollToApplyDeg);
+
+	M_AnimatedMesh->SetRelativeRotation(NewRotation);
+	M_LastAppliedRecoilPitchDeg = RecoilPitchToApplyDeg;
+	M_LastAppliedRecoilRollDeg = RecoilRollToApplyDeg;
+}
+
+void UAnimMeshFeedbackComponent::GetCurrentRecoilPitchAndRoll(
+	float& OutRecoilPitchDeg,
+	float& OutRecoilRollDeg) const
+{
+	OutRecoilPitchDeg = M_RecoilRotDeg.X;
+
+	if (M_AnimMeshRecoilAxesSettings.M_OptionalRollChannel == EAnimMeshOptionalRollChannel::Roll)
+	{
+		OutRecoilRollDeg = M_RecoilRotDeg.Z;
+		return;
+	}
+
+	OutRecoilRollDeg = 0.0f;
 }
