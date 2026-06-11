f74e1859e2c5127690b58b0febf3d9864ce57be6
Bas Blokzijl <79595891+Bblokz@users.noreply.github.com>
2026-06-11 11:37:38 +0200

    Add Spalliners technology effect

---
+++ RTS_Survival/TechTree/Technologies/GerTechnologies/Spalliners/TE_Spalliners.h
@@ -0,0 +1,28 @@
+﻿// Copyright (C) 2020-2025 Bas Blokzijl - All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RTS_Survival/TechTree/Technologies/TechnologyEffect/TechnologyEffect.h"
+#include "TE_Spalliners.generated.h"
+
+class ATankMaster;
+
+/**
+ * @brief Applies a survivability boost to researched medium German armour variants when the tech manager resolves them.
+ */
+UCLASS()
+class RTS_SURVIVAL_API UTE_Spalliners : public UTechnologyEffect
+{
+	GENERATED_BODY()
+
+public:
+	UTE_Spalliners();
+
+protected:
+	virtual void ApplyOnTank_Internal(ATankMaster* Tank) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Technology|Spalliners")
+	float M_HealthMultiplier;
+};
